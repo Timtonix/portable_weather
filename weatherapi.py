8dc71affda488b91412b0e0952a963a75c49648d
timtonix <timtonic@outlook.fr>
2023-01-07 11:02:21 +0100

    add current_termp() function which return the temp as integer

--- weatherapi.py
+++ weatherapi.py
@@ -7,9 +7,15 @@ class Weatherapi:
 
     def get_current_weather_from_api(self, aqi: str = "no") -> dict:
         response = urequests.get(url=f"https://api.weatherapi.com/v1/current.json?key={self.key}&q=Lille&aqi={aqi}")
-        return response
+        return response.json()
 
     def get_forecast_weather_from_api(self, aqi: str = "no", hour: int = None, days: int = 1) -> dict:
         response = urequests.get(url=f"https://api.weatherapi.com/v1/forecast.json?key={self.key}&q=Lille"
                                      f"&aqi={aqi}&days={days}&hour={hour}&lang=fr")
-        return response
+        return response.json()
+
+
+    def current_temp(self):
+        current_weather = self.get_current_weather_from_api()
+        current_temp = current_weather["current"]["temp_c"]
+        return current_temp
