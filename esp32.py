8e6f99dbf9ef9948d6e8a2454ed2aa75dbfb08e3
timtonix <timtonic@outlook.fr>
2023-01-08 08:39:54 +0100

    Fix #27 bugs an Portable Station v1 is finish !

--- esp32.py
+++ esp32.py
@@ -3,17 +3,16 @@ import ssd1306
 import time
 from machine import Pin, SoftI2C
 from weatherapi import Weatherapi
-import wifi
-from time import sleep, gmtime, localtime
-
-
-# Connexion au wifi
-wifi.do_connect()
+from time import sleep, localtime
+import ntptime
 
 # On récupère la clé d'API
 f = open('config.json')
 config = json.load(f)
 key = config["api_key"]
+
+# On synchronise la date
+ntptime.settime()
 
 # On initialise l'écran
 i2c = SoftI2C(scl=Pin(22), sda=Pin(21))
@@ -72,6 +71,7 @@ def display_weather_forecast_day(day: int = 1):
 def display_weather_forecast_hour(day: int = 1, hour: int = 1):
     date = localtime()
     following_hour = date[3] + hour
+    print(f"Hour = {date[3]}")
     if following_hour >= 24:
         following_hour = hour
         day += 1
@@ -96,15 +96,14 @@ while True:
         day += 1
         print(f"day {day}")
         display_weather_forecast_day(day)
-        sleep(3)
     elif yellow.value() == 0:
         hour += 1
         display_weather_forecast_hour(hour=hour)
-        sleep(3)
     elif red.value() == 0:
         welcome_message()
         day = 0
         hour = 0
+
 
 
 
